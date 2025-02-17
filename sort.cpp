a41dd39a3b1deb7fc9b2f29339150464fff5ac17
Minh Oanh <oanhpham41121@gmail.com>
2025-02-18 00:31:17 +0700

    counting sort

--- sort.cpp
+++ sort.cpp
@@ -47,29 +47,51 @@
 // }
 
 // 3. Sắp xếp chèn (insertion sort)
+// #include <iostream>
+// using namespace std;
+// void insertionSort(int a[], int n) {
+//   for (int i = 1; i < n; i++) {
+//     int j = i - 1, current = a[i];
+//     while (j >= 0 && a[j] > current) {
+//       a[j+1] = a[j];
+//       j--;
+//     }
+//     a[j+1] = current;
+//   }
+// }
+// int main() {
+//   int a[10] = {22, 41, 2, 3, 59, 121, 9, 8, 141, 6};
+//   insertionSort(a, 10);
+//   for (int x: a) {
+//     cout << x << " ";
+//   }
+//   cout << endl;
+//   return 0;
+// }
+
+// 4. Sắp xếp đếm phân phối (counting sort)
 #include <iostream>
 using namespace std;
-void insertionSort(int a[], int n) {
-  for (int i = 1; i < n; i++) {
-    int j = i - 1, current = a[i];
-    while (j >= 0 && a[j] > current) {
-      a[j+1] = a[j];
-      j--;
-    }
-    a[j+1] = current;
-  }
-}
 int main() {
-  int a[10] = {22, 41, 2, 3, 59, 121, 9, 8, 141, 6};
-  insertionSort(a, 10);
-  for (int x: a) {
-    cout << x << " ";
+  int count[1000001], m = INT_MIN;
+  int n;
+  cin >> n;
+  int a[n];
+  for (int &x: a) {
+    cin >> x;
+    count[x]++;
+    m = max(m, x);
+  }
+  for (int i = 0; i <= m; i++) {
+    if (count[i] != 0) {
+      for (int j = 0; j < count[i]; j++) {
+        cout << i << " ";
+      }
+    }
   }
   cout << endl;
   return 0;
 }
-
-// 4. Sắp xếp đếm phân phối (counting sort)
 
 // 5. Sắp xếp nhanh (quick sort)
 
