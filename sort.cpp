08857f19b8242a1023d6caf4ba56e123548d8a28
Minh Oanh <oanhpham41121@gmail.com>
2025-02-18 00:30:03 +0700

    insertion sort

--- sort.cpp
+++ sort.cpp
@@ -25,28 +25,49 @@
 // }
 
 // 2. Sắp xếp nổi bọt (bobble sort)
+// #include <iostream>
+// using namespace std;
+// void bobbleSort(int a[], int n) {
+//   for (int i = 0; i < n - 1; i++) {
+//     for (int j = 0; j < n - 1 - i; j++) {
+//       if (a[j] > a[j+1]) {
+//         swap(a[j], a[j+1]);
+//       }
+//     }
+//   }
+// }
+// int main() {
+//   int a[10] = {23, 56, 0, 32, 59, 111, 9, 7, 41, 2};
+//   bobbleSort(a, 10);
+//   for (int x: a) {
+//     cout << x << " ";
+//   }
+//   cout << endl;
+//   return 0;
+// }
+
+// 3. Sắp xếp chèn (insertion sort)
 #include <iostream>
 using namespace std;
-void bobbleSort(int a[], int n) {
-  for (int i = 0; i < n - 1; i++) {
-    for (int j = 0; j < n - 1 - i; j++) {
-      if (a[j] > a[j+1]) {
-        swap(a[j], a[j+1]);
-      }
+void insertionSort(int a[], int n) {
+  for (int i = 1; i < n; i++) {
+    int j = i - 1, current = a[i];
+    while (j >= 0 && a[j] > current) {
+      a[j+1] = a[j];
+      j--;
     }
+    a[j+1] = current;
   }
 }
 int main() {
-  int a[10] = {23, 56, 0, 32, 59, 111, 9, 7, 41, 2};
-  bobbleSort(a, 10);
+  int a[10] = {22, 41, 2, 3, 59, 121, 9, 8, 141, 6};
+  insertionSort(a, 10);
   for (int x: a) {
     cout << x << " ";
   }
   cout << endl;
   return 0;
 }
-
-// 3. Sắp xếp chèn (insertion sort)
 
 // 4. Sắp xếp đếm phân phối (counting sort)
 
