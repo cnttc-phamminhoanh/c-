6e938226165bbdc2533f639b7a95f8424548a98a
Minh Oanh <oanhpham41121@gmail.com>
2025-02-18 22:44:10 +0700

    number of occurrences

--- binarySearch.cpp
+++ binarySearch.cpp
@@ -50,13 +50,56 @@
 // }
 
 // 3. Tìm vị trí cuối cùng của phần tử trong mảng đã sắp xếp, không tìm thấy trả về - 1
+// #include <iostream>
+// using namespace std;
+// int lastPositionSearch(int a[], int n, int target) {
+//   int l = 0, r = n - 1, res = -1;
+//   while (l <= r) {
+//     int mid = (l + r) / 2;
+//     if (a[mid] == target) {
+//       res = mid;
+//       l = mid + 1;
+//     }
+//     else if (target > a[mid]) {
+//       l = mid + 1;
+//     }
+//     else {
+//       r = mid - 1;
+//     }
+//   }
+//   return res;
+// }
+// int main() {
+//   int a[10] = {1, 2, 2, 4, 4, 4, 6, 7, 8, 9};
+//   cout << lastPositionSearch(a, 10, 4) << endl;
+//   return 0;
+// }
+
+// 4. In số lần xuất hiện của phần tử trong mảng đã sắp xếp, không có thì in ra 0
 #include <iostream>
 using namespace std;
+int firstPositionSearch(int a[], int n, int target) {
+  int l = 0, r = n - 1, res = -1;
+  while (l <= r) {
+    int mid = (l + r) / 2;
+    if (target == a[mid]) {
+      res = mid;
+      r = mid - 1;
+    }
+    else if (target > a[mid]) {
+      l = mid + 1;
+    }
+    else {
+      r = mid - 1;
+    }
+  }
+  return res;
+}
 int lastPositionSearch(int a[], int n, int target) {
   int l = 0, r = n - 1, res = -1;
   while (l <= r) {
     int mid = (l + r) / 2;
-    if (a[mid] == target) {
+    if (target == a[mid]) {
       res = mid;
       l = mid + 1;
     }
@@ -70,7 +113,14 @@ int lastPositionSearch(int a[], int n, int target) {
   return res;
 }
 int main() {
-  int a[10] = {1, 2, 2, 4, 4, 4, 6, 7, 8, 9};
-  cout << lastPositionSearch(a, 10, 4) << endl;
+  int a[10] = {1, 2, 2, 3, 3, 3, 3, 3, 8, 9};
+  int l = firstPositionSearch(a, 10, 3);
+  int r = lastPositionSearch(a, 10, 3);
+  if (l != -1) {
+    cout << r - l + 1 << endl;
+  }
+  else {
+    cout << "0" << endl;
+  }
   return 0;
 }
