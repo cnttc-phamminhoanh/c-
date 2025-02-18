577d0ce64cfb7e277794237aea47174596f769cb
Minh Oanh <oanhpham41121@gmail.com>
2025-02-18 17:12:15 +0700

    quick sort

--- sort.cpp
+++ sort.cpp
@@ -94,52 +94,113 @@
 // }
 
 // 5. Sắp xếp trộn (merge sort)
+// #include <iostream>
+// using namespace std;
+// void merge(int a[], int l, int m, int r) {
+//   vector<int> x(a + l, a + m + 1);
+//   vector<int> y(a + m + 1, a + r + 1);
+//   int i = 0, j = 0;
+//   while (i < x.size() && j < y.size()) {
+//     if (x[i] < y[j]) {
+//       a[l] = x[i];
+//       l++;
+//       i++;
+//     }
+//     else {
+//       a[l] = y[j];
+//       l++;
+//       j++;
+//     }
+//   }
+//   while (i < x.size()) {
+//     a[l] = x[i];
+//     l++;
+//     i++;
+//   }
+//   while (j < y.size()) {
+//     a[l] = y[j];
+//     l++;
+//     j++;
+//   }
+// }
+// void mergeSort(int a[], int l, int r) {
+//   if (l >= r) return;
+//   int m = (l + r) / 2;
+//   mergeSort(a, l, m);
+//   mergeSort(a, m + 1, r);
+//   merge(a, l, m, r);
+// }
+// int main() {
+//   int a[10] = {55, 4, 2, 67, 333, 9, 7, 7, 1, 80};
+//   mergeSort(a, 0, 9);
+//   for (int x: a) {
+//     cout << x << " ";
+//   }
+//   cout << endl;
+//   return 0;
+// }
+
+// 6. Sắp xếp nhanh (quick sort)
 #include <iostream>
 using namespace std;
-void merge(int a[], int l, int m, int r) {
-  vector<int> x(a + l, a + m + 1);
-  vector<int> y(a + m + 1, a + r + 1);
-  int i = 0, j = 0;
-  while (i < x.size() && j < y.size()) {
-    if (x[i] < y[j]) {
-      a[l] = x[i];
-      l++;
+int lomutoPartition(int a[], int l, int r) {
+  int pivot = a[r];
+  int i = l - 1;
+  for (int j = l; j < r; j++) {
+    if (a[j] <= pivot) {
+      i++;
+      swap(a[i], a[j]);
+    }
+  }
+  swap(a[i+1], a[r]);
+  return i + 1;
+}
+void quickSortWithLomuto(int a[], int l, int r) {
+  if (l >= r) return;
+  int p = lomutoPartition(a, l, r);
+  quickSortWithLomuto(a, l, p - 1);
+  quickSortWithLomuto(a, p + 1, r);
+}
+int hoarePartition(int a[], int l, int r) {
+  int pivot = a[l];
+  int i = l - 1, j = r + 1;
+  while (true) {
+    do {
       i++;
     }
+    while (a[i] < pivot);
+    do {
+      j--;
+    }
+    while (a[j] > pivot);
+    if (i < j) {
+      swap(a[i], a[j]);
+    }
     else {
-      a[l] = y[j];
-      l++;
-      j++;
+      return j;
     }
   }
-  while (i < x.size()) {
-    a[l] = x[i];
-    l++;
-    i++;
-  }
-  while (j < y.size()) {
-    a[l] = y[j];
-    l++;
-    j++;
-  }
 }
-void mergeSort(int a[], int l, int r) {
+void quickSortWithHoare(int a[], int l, int r) {
   if (l >= r) return;
-  int m = (l + r) / 2;
-  mergeSort(a, l, m);
-  mergeSort(a, m + 1, r);
-  merge(a, l, m, r);
+  int p = hoarePartition(a, l, r);
+  quickSortWithHoare(a, l, p);
+  quickSortWithHoare(a, p + 1, r);
 }
 int main() {
-  int a[10] = {55, 4, 2, 67, 333, 9, 7, 7, 1, 80};
-  mergeSort(a, 0, 9);
+  int a[10] = {44, 3, 3, 6, 0, 11, 9, 7, 2, 1};
+  quickSortWithLomuto(a, 0, 9);
   for (int x: a) {
+    cout << x << " ";
+  }
+  cout << endl;
+  int b[10] = {44, 3, 30, 6, 0, 15, 9, 71, 1, 1};
+  quickSortWithHoare(b, 0, 9);
+  for (int x: b) {
     cout << x << " ";
   }
   cout << endl;
   return 0;
 }
-
-// 6. Sắp xếp nhanh (quick sort)
 
 // 7. Sắp xếp vun đống (heap sort)
