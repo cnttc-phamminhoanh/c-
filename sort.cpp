f36480922289b238f05dee68cde9500bab8412b3
Minh Oanh <oanhpham41121@gmail.com>
2025-02-18 16:38:45 +0700

    merge sort

--- sort.cpp
+++ sort.cpp
@@ -70,29 +70,76 @@
 // }
 
 // 4. Sắp xếp đếm phân phối (counting sort)
+// #include <iostream>
+// using namespace std;
+// int main() {
+//   int count[1000001], m = INT_MIN;
+//   int n;
+//   cin >> n;
+//   int a[n];
+//   for (int &x: a) {
+//     cin >> x;
+//     count[x]++;
+//     m = max(m, x);
+//   }
+//   for (int i = 0; i <= m; i++) {
+//     if (count[i] != 0) {
+//       for (int j = 0; j < count[i]; j++) {
+//         cout << i << " ";
+//       }
+//     }
+//   }
+//   cout << endl;
+//   return 0;
+// }
+
+// 5. Sắp xếp trộn (merge sort)
 #include <iostream>
 using namespace std;
-int main() {
-  int count[1000001], m = INT_MIN;
-  int n;
-  cin >> n;
-  int a[n];
-  for (int &x: a) {
-    cin >> x;
-    count[x]++;
-    m = max(m, x);
-  }
-  for (int i = 0; i <= m; i++) {
-    if (count[i] != 0) {
-      for (int j = 0; j < count[i]; j++) {
-        cout << i << " ";
-      }
+void merge(int a[], int l, int m, int r) {
+  vector<int> x(a + l, a + m + 1);
+  vector<int> y(a + m + 1, a + r + 1);
+  int i = 0, j = 0;
+  while (i < x.size() && j < y.size()) {
+    if (x[i] < y[j]) {
+      a[l] = x[i];
+      l++;
+      i++;
     }
+    else {
+      a[l] = y[j];
+      l++;
+      j++;
+    }
+  }
+  while (i < x.size()) {
+    a[l] = x[i];
+    l++;
+    i++;
+  }
+  while (j < y.size()) {
+    a[l] = y[j];
+    l++;
+    j++;
+  }
+}
+void mergeSort(int a[], int l, int r) {
+  if (l >= r) return;
+  int m = (l + r) / 2;
+  mergeSort(a, l, m);
+  mergeSort(a, m + 1, r);
+  merge(a, l, m, r);
+}
+int main() {
+  int a[10] = {55, 4, 2, 67, 333, 9, 7, 7, 1, 80};
+  mergeSort(a, 0, 9);
+  for (int x: a) {
+    cout << x << " ";
   }
   cout << endl;
   return 0;
 }
 
-// 5. Sắp xếp nhanh (quick sort)
+// 6. Sắp xếp nhanh (quick sort)
 
-// 6. Sắp xếp vun đống (heap sort)
+// 7. Sắp xếp vun đống (heap sort)
