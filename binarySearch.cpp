4ad6548a574785cea2f7a1f8aa56cff472e80651
Minh Oanh <oanhpham41121@gmail.com>
2025-02-18 22:05:23 +0700

    binary search

---
+++ binarySearch.cpp
@@ -0,0 +1,24 @@
+// 1. Tìm kiếm phần tử bằng thuật toán nhị phân, nếu có trả về true, không tìm thấy trả về false
+#include <iostream>
+using namespace std;
+bool binarySearch(int a[], int n, int target) {
+  int l = 0, r = n - 1;
+  while (l <= r) {
+    int mid = (l + r) / 2;
+    if (target == a[mid]) {
+      return true;
+    }
+    else if (target > a[mid]) {
+      l = mid + 1;
+    }
+    else {
+      r = mid - 1;
+    }
+  }
+  return false;
+}
+int main() {
+  int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  cout << boolalpha << binarySearch(a, 10, 3) << endl;
+  return 0;
+}
